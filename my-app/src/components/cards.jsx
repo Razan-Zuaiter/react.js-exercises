a53840eb3fb2ab5121c178933ed3de4f34bcf012
Razan-Zuaiter <zuaiter.razan@gmail.com>
2020-11-21 23:02:36 +0200

    react

--- my-app/src/components/cards.jsx
+++ my-app/src/components/cards.jsx
@@ -10,6 +10,14 @@ import Img7 from "../assets/heba.jpg";
 import Img8 from "../assets/jenan.jpg";
 import Img9 from "../assets/waed.jpg";
 import Img10 from "../assets/hala.png";
+import Img11 from "../assets/dana.jpg";
+import Img12 from "../assets/orange.jpg";
+import Img13 from "../assets/orange.jpg";
+import Img14 from "../assets/razan.jpg";
+import ImgA from "../assets/khadeeja.jpg";
+import ImgB from "../assets/amal.jpg";
+
+
 
 class Cards extends Component {
   state = {};
@@ -17,16 +25,50 @@ class Cards extends Component {
     return (
       <div className="row container-fluid d-flex justify-content-center">
         <h2 class="display-4">
-        Meet up our most elegant and cutest developers ever <hr></hr>{" "}
-          <h3>
-            {" "}
-            <em> The world needs girl coders to lead it</em>{" "}
-          </h3>
+          Meet up our most elegant and cutest developers ever <hr></hr>{" "}<h3>
+          {" "}
+          <em> The world needs girl coders to lead it</em>{" "}
+        </h3>
         </h2>
-       
+        
         <br></br>
         <br></br>
+        
+          <div className="row ">
+            <div className="col-6 cardA">
+              <Card
+                imgsrc={ImgA}
+                title="Khadeeja"
+                text="Expert trainer "
+                linked="https://www.linkedin.com/in/khadeejah-hamdan"
+                git="https://github.com/khadeejah-hamdan"
+              />
+            </div>
+            <div className="col-4 cardA">
+              <Card
+                imgsrc={ImgB}
+                title="Amal"
+                text="Assistant trainer"
+                linked="https://www.linkedin.com/in/amal-al-dajah-b5b423193"
+                git="https://github.com/AmalAldajah"
+              />
+            </div>{" "}
+         
+        </div>
+        <br></br>
+        <br></br>
+
         <div className="row">
+          <div className="col">
+            <Card
+              imgsrc={Img14}
+              title="Razan"
+              text=" Communication Engineer and developer  "
+              linked="www.linkedin.com/in/razan-zuaiter"
+              git="https://github.com/Razan-Zuaiter"
+            />
+          </div>
+
           <div className="col">
             <Card
               imgsrc={Img1}
@@ -58,18 +100,10 @@ class Cards extends Component {
             <Card
               imgsrc={Img4}
               title="Lubna"
-              text=" MIS and software developer "
+              text="Financial information System
+              and developer "
               linked="https://www.linkedin.com/in/lubna-almaa"
               git="https://github.com/lubna-almaaweed"
-            />
-          </div>
-          <div className="col">
-            <Card
-              imgsrc={Img5}
-              title="Sara"
-              text=" MIS and Developer"
-              linked="https://www.linkedin.com/in/sarah-eswed"
-              git="https://github.com/Sarah-Eswed"
             />
           </div>
         </div>
@@ -125,7 +159,47 @@ class Cards extends Component {
             />
           </div>
         </div>
-        
+        <div className="row">
+          <div className="col">
+            <Card
+              imgsrc={Img11}
+              title="Dana"
+              text=" Software Engineer and developer  "
+              linked="https://www.linkedin.com/in/dana-toughoj/
+             "
+              git="https://github.com/danatoughoj"
+            />
+          </div>
+
+          <div className="col">
+            <Card
+              imgsrc={Img12}
+              title="Saja"
+              text=" Electrical Engineer and developer  "
+              linked="https://www.linkedin.com/in/saja-dahamsheh/"
+              git="https://github.com/sajadahamsheh"
+            />
+          </div>
+
+          <div className="col">
+            <Card
+              imgsrc={Img13}
+              title="Leen"
+              text=" chemistry and  software developer  "
+              linked="https://www.linkedin.com/in/leen-awad/"
+              git="https://github.com/leen-awad"
+            />
+          </div>
+          <div className="col">
+            <Card
+              imgsrc={Img5}
+              title="Sara"
+              text=" MIS and Developer"
+              linked="https://www.linkedin.com/in/sarah-eswed"
+              git="https://github.com/Sarah-Eswed"
+            />
+          </div>
+        </div>
       </div>
     );
   }
